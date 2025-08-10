f340be4c557e423e8fb5aec898fa59fdc2677e00
ketelenleticia <ketelen.lima10@gmail.com>
2025-08-09 22:57:06 -0300

    logica.py

---
+++ inf.py
@@ -0,0 +1,83 @@
+# QUESTÃO 1 
+#for n in range(1,11):
+#    print(f" os valor n no range:{n}")
+
+# QUESTÃO 2
+#pedirnumero=10
+#while pedirnumero >=1:
+#    print(F"o numero:{pedirnumero}")
+#    pedirnumero-=1
+#print("fim da repetição")
+
+# QUESTÃO 3
+#nome=input(" digite seu nome:")
+#for nome1 in range(1,6):
+#    print(f"hola,bienvenido(a) {nome} ")
+
+# QUESTÃO 4
+#print("os valores pares de 0 a 20 são:")
+#for n in range(0,21,2):
+#    print(f"numero:{n}")
+#print("fim da repetição!")
+
+# QUESTÃO 5 
+c=0
+while True:
+    s=int(input("digite uma senha de 4 digitos:"))
+    if s==1234:
+        print("senha correta,seja bem-vindo ao programa")
+        break
+    else:
+          print("senha incorreta,tente novamente")
+    
+
+
+# QUESTÃO 6 (pesquisada)
+#soma =0
+#for i in range(1,6):
+#    n =int(input(f"digite o numero: "))
+#    soma+=n
+#print(f"a soma total é : {soma}")
+
+# QUESTÃO 7
+#for numero in range(1,11):
+#    if numero % 2 ==0:
+#        continue
+#    print(numero)
+
+# QUESTÃO 8 (pesquisada)
+#c=0
+#while True:
+#    n=int(input("digite um numero para sair do progama:"))
+#    if n==0:
+#        break
+#    c+=1
+#print(f"voce digitou {c} numeros,para sair do programa")
+
+# QUESTÃO 9
+#n=int(input("digite um numero para ver a tabuada:"))
+#print(f"Tabuada do {n}:")
+#for i in range(1,11):
+#    resul=n * i
+#    print(f"{n} x {i} = {resul}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+                
+ 
