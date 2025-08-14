56dcfc70a22d20c6b7aed6ec3ca487d6ef4b2ced
kete <ketelen.lima10@gmail.com>
2025-08-14 06:49:28 -0300

    Add files via upload

---
+++ inp2.py
@@ -0,0 +1,69 @@
+# QUESTÃO 1
+#n1=float(input("digite uma nota:"))
+#n2=float(input("digite outra nota:"))
+#n3=float(input("digite u ma outra nota:"))
+#m=(n1+n2+n3)/3
+#print(f"{m:.2f}")
+#if m >=7:
+#    print("aprovado")
+#else:
+#    print("reprovado")
+
+# QUETÃO 2
+#n=str(input("digite uma palavra:"))
+#cv=0
+#for i in (n):
+#    if i=="a" or i=="e" or i== "i" or i=="o" or i=="u":
+#     cv+=1
+#print(f"total de vogais:{cv}")      
+
+#  QUESTÃO 3
+#n=int(input("digite um numero:"))
+#fat=1
+#for i in range(1,n+1):
+#   fat*=i
+#print(F"o fatorial de {n} é {fat}")
+
+# QUESTÃO 4
+#n1=int(input("Digite um numero,para saber se é impar ou par:"))
+#if n1 % 2==0:
+#    print("o numero é par")
+#else:
+#    print("o numero é impar")
+
+# QUESTÃO 5
+#n=[]
+#for i  in range(5):
+#    n1=float(input(f"digite o {i + 1} numero:"))
+#    n.append(n1)
+#print("maior numero:",max(n))    
+#print("menor numero:",min(n))
+
+# QUESTÃO 6
+#total=0
+#while True:
+#    p=float(input("digite o preço do produto (ou 0 para finalizar): R$"))
+#    if p==0:
+#        break
+#    total+=p
+#print(f"\ntotal a pagar: R$ {total:.2f}")
+
+# QUESTÃO 7
+#n=int(input("digite um numero para calcular o fatorial:"))
+#c=n
+#f=1
+#print('calculo {}!='.format(n), end='')
+#while c>0:
+#  print("{}".format(c), end='')
+#  print('x'if c>1 else '=',end='')
+#  f*=c
+#  c-=1
+#print("{}".format(f))
+
+
+
+
+
+      
+
+
